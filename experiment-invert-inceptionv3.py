3cfe32d83e33cf10c0e85bb36269e3c53e6295cd
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2022-06-23 15:31:54 +0100

    inception: choose classes or embedding (embedding doesnt work)

--- experiment-invert-inceptionv3.py
+++ experiment-invert-inceptionv3.py
@@ -1,30 +1,51 @@
-from multiprocessing.sharedctypes import Value
 import sys
+import argparse
+
 import numpy
+
 import torch
 from torch import nn
-from input_optimizer import ModelInverter
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import save_image
 
-default_image_class = 941
-image_class = default_image_class
-if(len(sys.argv) > 1):
-    image_class = sys.argv[1]
-    try:
-        image_class = int(image_class)
-    except ValueError:
-        image_class = default_image_class
-    else:
-        if((image_class < 0) or (image_class >= 1000)):
-            image_class = default_image_class
+from input_optimizer import ModelInverter
 
-torch_device = "cuda" if torch.cuda.is_available() else "cpu"
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--model-type', type=str, default='classes')
+argparser.add_argument('--image-name', type=str, default='')
+argparser.add_argument('--image-class', type=int, default=941)
+argparser.add_argument('--device', type=str, default='cuda')
+parsed_arguments = argparser.parse_args()
+
+assert(parsed_arguments.device in [ 'cuda', 'cpu' ])
+
+print('command line arguments values:')
+print(parsed_arguments)
+
+
+image_name = parsed_arguments.image_name
+model_type = parsed_arguments.model_type
+assert(model_type in [ 'embedding', 'classes' ])
+
+if(model_type == 'classes'):
+    image_class = parsed_arguments.image_class
+    assert(image_class >= 0)
+    assert(image_class < 1000)
+    if(image_name == ''):
+        image_name = f'{image_class}'
+
+if(parsed_arguments.device == 'cuda'):
+    torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using {torch_device} device')
 
 tensorboard_writer = SummaryWriter()
 
-model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True).to(torch_device)
+model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
+if(model_type == 'embedding'):
+    print(f'model type is {model_type}: removing the last layers')
+    inception_layers = list(model.children())
+    model = torch.nn.Sequential(*(inception_layers[:-2]))
+model.to(torch_device)
 
 norm_mean = [ 0.485, 0.456, 0.406 ]
 norm_std = [ 0.229, 0.224, 0.225 ]
@@ -40,21 +61,27 @@ input_vector = (torch.rand((1, 3, 299, 299),
                            device=torch_device,
                            requires_grad=True) - norm_mean) / norm_std
 
-save_image(input_vector[0], f'initial-image-{image_class}.png')
+save_image(input_vector[0], f'initial-image-{image_name}.png')
 
 #input_vector.to(torch_device)
 
 model.eval()
-nothing = model(input_vector)
+
+print(input_vector.shape)
+
+initial_outputs = model(input_vector)
 # input shape: batch_size x 3 x 299 x 299
 # output shape: batch_size x 1000
 
-expected_output_vector = numpy.zeros(shape=nothing.shape)
-expected_output_vector[0, image_class] = 1.
-expected_output_vector = torch.tensor(expected_output_vector,
-                                      dtype=torch.float,
-                                      device=torch_device)
+expected_output_vector = numpy.zeros(shape=initial_outputs.shape)
 
+if(model_type == 'classes'):
+    expected_output_vector[0, image_class] = 1.
+    expected_output_vector = torch.tensor(expected_output_vector,
+                                        dtype=torch.float,
+                                        device=torch_device)
+else:
+    expected_output_vector += torch.rand_like(expected_output_vector)
 
 model_inverter = ModelInverter(model,
                                input_vector,
@@ -77,4 +104,4 @@ output = model(generated_image)
 print(output)
 print(expected_output_vector)
 
-save_image(generated_image[0], f'generated-image-{image_class}.png')
+save_image(generated_image[0], f'generated-image-{image_name}.png')
