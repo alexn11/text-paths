36d7510cce8e899c485e5edc3780f78b214ab494
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2022-06-23 16:57:45 +0100

    invert inception with regularization

--- invert_inceptionv3.py
+++ invert_inceptionv3.py
@@ -10,11 +10,19 @@ from torchvision.utils import save_image
 
 from input_optimizer import ModelInverter
 
+# weight decay 0 -> max steps 20000+
+# wieght decay 0.01 -> max steps: 1000
+# weight decay > 0.1 -> max steps ~500
+
+
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--model-type', type=str, default='classes')
 argparser.add_argument('--image-name', type=str, default='')
 argparser.add_argument('--image-class', type=int, default=941)
 argparser.add_argument('--device', type=str, default='cuda')
+argparser.add_argument('--weight-decay', type=float, default=0.5) # 0.01
+argparser.add_argument('--inverter-nb-max-steps', type=int, default=500) # 1000 # 20000
+argparser.add_argument('--do-save-initial-state', type=bool, default=False)
 parsed_arguments = argparser.parse_args()
 
 assert(parsed_arguments.device in [ 'cuda', 'cpu' ])
@@ -33,6 +41,13 @@ if(model_type == 'classes'):
     assert(image_class < 1000)
     if(image_name == ''):
         image_name = f'{image_class}'
+
+if(image_name == ''):
+    image_name = 'noname'
+
+weight_decay = parsed_arguments.weight_decay
+inverter_nb_max_steps = parsed_arguments.inverter_nb_max_steps
+do_save_initial_state = parsed_arguments.do_save_initial_state
 
 if(parsed_arguments.device == 'cuda'):
     torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -67,13 +82,10 @@ input_vector = (torch.rand((1, 3, 299, 299),
                            device=torch_device,
                            requires_grad=True) - norm_mean) / norm_std
 
-save_image(input_vector[0], f'initial-image-{image_name}.png')
-
-#input_vector.to(torch_device)
+if(do_save_initial_state):
+    save_image(input_vector[0], f'initial-image-{image_name}.png')
 
 model.eval()
-
-print(input_vector.shape)
 
 initial_outputs = model(input_vector)
 # input shape: batch_size x 3 x 299 x 299
@@ -84,19 +96,23 @@ expected_output_vector = torch.zeros_like(initial_outputs)
 if(model_type == 'classes'):
     expected_output_vector[0, image_class] = 1.
     expected_output_vector = torch.tensor(expected_output_vector,
-                                        dtype=torch.float,
-                                        device=torch_device)
+                                          dtype=torch.float,
+                                          device=torch_device)
 else:
     expected_output_vector += torch.rand_like(expected_output_vector)
 
 model_inverter = ModelInverter(model,
                                input_vector,
                                loss_function=nn.MSELoss(reduction='sum'),
+                               torch_device=torch_device,
                                tensorboard_writer=tensorboard_writer)
+
 loss_history = model_inverter.compute_inverse(expected_output_vector,
-                                              n_max_steps=20000,
-                                              torch_device=torch_device,
-                                              lr=0.1)
+                                              n_max_steps=inverter_nb_max_steps,
+                                              lr=0.1,
+                                              optimizer_kwargs={
+                                                'weight_decay': weight_decay,
+                                              })
 # lr experiments:
 # 0.001 smooth slow
 # 0.1 faster still smooth slow < best
@@ -106,8 +122,9 @@ loss_history = model_inverter.compute_inverse(expected_output_vector,
 
 
 generated_image = model_inverter.get_computed_solution()
-output = model(generated_image)
-print(output)
-print(expected_output_vector)
+
+#output = model(generated_image)
+#print(output)
+#print(expected_output_vector)
 
 save_image(generated_image[0], f'generated-image-{image_name}.png')
