a94b36e4c2e7a742ddaa07a8e9a87276f5882a7c
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2022-06-23 15:32:15 +0100

    rm debug things

--- bert_text_path.py
+++ bert_text_path.py
@@ -18,6 +18,9 @@ argparser.add_argument('--core-small-loss', type=float, default=13500)#11080)
 argparser.add_argument('--nb-interpolation-steps', type=int, default=21)
 parsed_arguments = argparser.parse_args()
 
+print('command line arguments values:')
+print(parsed_arguments)
+
 learning_rate = parsed_arguments.lr
 torch_device = parsed_arguments.device
 core_n_max_steps = parsed_arguments.core_n_max_steps
@@ -37,12 +40,7 @@ start_sentence = 'There is no place like home.'
 end_sentence_one_word = 'There is no place like Italy.'
 end_sentence_keep_meaning = 'No place is like home.'
 end_sentence_different = 'The cat jumps over the fence.'
-# DEBUG:
-#nb_interpolation_steps = 1 # 2
-# <<<<<
-#core_small_loss = 0.1
-#core_n_max_steps = 1220
-##core_n_max_steps = 48 #12 # 122
+
 embedding_small_loss = 10
 embedding_n_max_steps = 12 # 212
 
@@ -74,13 +72,7 @@ solver = ModelInverter(core_model,
                        loss_function=core_bert_loss_function,
                        tensorboard_writer=tensorboard_writer)
 
-# >
 for i in range(nb_interpolation_steps):
-  # DEBUG:
-  print(f'embed vect loop: {i}')
-  # <<<
-  #i = 0
-  #if(True):
   solver.compute_inverse(outputs[i],
                          n_max_steps=core_n_max_steps,
                          min_loss=core_small_loss,
@@ -88,26 +80,14 @@ for i in range(nb_interpolation_steps):
                          lr=learning_rate)#lr=0.01)#lr=0.0001) # DEBUG: 0.01<ok, 1.<bad, 0.1<bad, 0.02 < ok?
   embedding_vectors.append(solver.get_computed_solution())
 
-# DEBUG:
-#print('computed embedding vectors:')
-#for i, v in enumerate(embedding_vectors):
-#  print(f'{i}: {v}')
-# <<<<
 
 # embedding path
 
 input_vectors = [ start_emb_input ]
 
 solver = ModelInverter(bert_modules.get_embedding_model(), start_emb_input.clone().detach())
-solver.to(torch_device)
 
-# >
 for i in range(nb_interpolation_steps):
-  # DEBUG:
-  print(f'input vect loop: {i}')
-  # <<<
-  #i = 0
-  #if(True):
   solver.compute_inverse(embedding_vectors[i],
                          n_max_steps=embedding_n_max_steps,
                          min_loss=embedding_small_loss,
@@ -115,19 +95,18 @@ for i in range(nb_interpolation_steps):
                          lr=0.01)
   input_vectors.append(solver.get_computed_solution())
 
-# DEBUG:
-#print('computed input vectors:')
-#for i, v in enumerate(input_vectors):
-#  print(f'{i}: {v}')
-# <<<<
 
 # text path
-# TODO translate the sequence of 1hot vectors into text (check how to use the Tokenizer)
+
 def remove_special_tokens(text):
+  text = text.split('.')[0]
+  # could probably use regex instead
   text = text.replace('[PAD] ', '')
   text = text.replace('[PAD]', '')
   text = text.replace('[CLS] ', '')
   text = text.replace('[SEP] ', '')
+  text = text.replace('[MASK] ', '')
+  text = text.replace('[MASK]', '')
   return text
 
 def translate_inputs_to_text(tokenizer, input_vector):
@@ -150,7 +129,6 @@ for input_vector in input_vectors:
   text = translate_inputs_to_text(tokenizer, input_vector)
   texts.append(text)
 
-# TODO: print the texts
 for i, text in enumerate(texts):
   print(f'{i}: {text}')
 
